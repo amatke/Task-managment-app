03d5e35d726024dc14bc84827258a001dccb13ea
lexam <lexam@LAPTOP-BPRBOSE2>
2020-04-19 13:55:07 +0200

    Disable spring security login

--- TaskManagmentApp/src/main/java/com/matovic/TaskManagmentAppApplication.java
+++ TaskManagmentApp/src/main/java/com/matovic/TaskManagmentAppApplication.java
@@ -2,8 +2,9 @@ package com.matovic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TaskManagmentAppApplication {
 
 	public static void main(String[] args) {
